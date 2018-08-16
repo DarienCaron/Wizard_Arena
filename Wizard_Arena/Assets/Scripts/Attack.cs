5edc3a68146def6353b12620f20b096862d0ef22
Darien Caron <caro0103@algonquinlive.com>
2018-08-16 12:25:08 -0400

    Adding Attacks

---
+++ Wizard_Arena/Assets/Scripts/Attack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+    public GameObject HitTarget;
+
+    public float Damage;
+
+}
+
+
+
