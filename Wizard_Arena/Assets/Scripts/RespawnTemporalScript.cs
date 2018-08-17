c8f153d9f34de773fd1fdb5e737d07708ff79055
nicozobnin <nicozobnin10b@gmail.com>
2018-08-16 20:37:00 -0400

    Material / Physics material / Shader
    Material: some materials, like sand and brick walls. simple materials
    Physics material:have different materials, with diffrent values, (wood, metal, rubber, etc)
    Shader: some simple shaders (examples, should expand)

---
+++ Wizard_Arena/Assets/Scripts/RespawnTemporalScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnTemporalScript : MonoBehaviour
+{
+    public Vector3 _InitialPos; // Starting position of the player
+    public float _YOffset;
+    public Rigidbody rb;
+    void Start()
+    {
+        _InitialPos = transform.position;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Vector3 tempVel = rb.velocity;
+            rb.velocity = tempVel + new Vector3(0, 10, 0);
+            print("space key was pressed");
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            Vector3 tempVel = rb.velocity;
+            rb.velocity = tempVel + new Vector3(0, 0, 10);
+            print("space key was pressed");
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            Vector3 tempVel = rb.velocity;
+            rb.velocity = tempVel + new Vector3(-10, 0, 0);
+            print("space key was pressed");
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            Vector3 tempVel = rb.velocity;
+            rb.velocity = tempVel + new Vector3(0, 0, -10);
+            print("space key was pressed");
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            Vector3 tempVel = rb.velocity;
+            rb.velocity = tempVel + new Vector3(10, 0, 0);
+            print("space key was pressed");
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        if (transform.position.y < _YOffset)
+        {
+            Debug.Log("the object is under the limit");
+            transform.position = _InitialPos;
+        }
+    }
+}
