e23b34bb852313eed2a9cbee083a352fd62b55d2
nicozobnin <nicozobnin10b@gmail.com>
2018-08-22 03:42:25 -0400

    sound/ music manager implemented
    there is a sounds/ music manager implemented
    right now is only working as a music manager, if needed i can implement a second one so it can also handle a more dinamyc way to handle the sound.

---
+++ Wizard_Arena/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    //  AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+
+
+
+    public AudioSource audioBackgorundMusic;
+    public AudioClip[] backggroundMusic;
+    private int musicIndex;
+
+
+    [Range(0.0f, 1.0f)]
+    public float volume;
+    [Range(0.0f, 3.0f)]
+    public float pitch;
+    // Use this for initialization
+    void Start ()
+    {
+
+
+    }
+
+    private void Awake()
+    {
+        audioBackgorundMusic = gameObject.AddComponent<AudioSource>();
+
+
+
+        musicIndex = 0;
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+            audioBackgorundMusic.clip = backggroundMusic[musicIndex];
+            audioBackgorundMusic.volume = volume;
+            audioBackgorundMusic.pitch = pitch;
+            audioBackgorundMusic.Play();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("next song requested");
+            LoadNextSong();
+        }
+        if (!audioBackgorundMusic.isPlaying)
+        {
+            LoadNextSong();
+        }
+
+    }
+    public void LoadNextSong()
+    {
+        int maxIndex = backggroundMusic.Length;
+        musicIndex++;
+        if (musicIndex >= maxIndex)
+        {
+            musicIndex = 0;
+        }
+        audioBackgorundMusic.Stop();
+        audioBackgorundMusic.clip = backggroundMusic[musicIndex];
+        audioBackgorundMusic.Play();
+        //SceneManager.GetActiveScene().buildIndex;
+
+    }
+
+    public void ChangeBackGroundMusic(AudioClip aMusic)
+    {
+        audioBackgorundMusic.Stop();
+        audioBackgorundMusic.clip = aMusic;
+    }
+}
---
+++ Wizard_Arena/Assets/Scripts/SoundObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class SoundObject : MonoBehaviour
+{
+    public string clipName;
+
+    public AudioClip clipItem;
+    [Range(0.0f, 1.0f)]
+    public float volume;
+    [Range(0.0f, 3.0f)]
+    public float pitch;
+
+}
