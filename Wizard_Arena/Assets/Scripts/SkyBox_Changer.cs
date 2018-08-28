7d105004bb9040cc468cd2c88b25827ecf081da0
Darien Caron <caro0103@algonquinlive.com>
2018-08-27 21:25:51 -0400

    SkyBox Changes
    Added 2 skybox materials and there respective textures. In each scene i also included a prefab that allows you to choose what skybox will be in the scene.

---
+++ Wizard_Arena/Assets/Scripts/SkyBox_Changer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBox_Changer : MonoBehaviour {
+     public enum SkyBoxes
+    {
+        Evening,
+        Night
+    }
+
+    public SkyBoxes SkyBoxChoice;
+
+    public Material Evening;
+    public Material Night;
+	// Use this for initialization
+	void Start () {
+		
+        if(SkyBoxChoice == SkyBoxes.Evening)
+        {
+            RenderSettings.skybox = Evening;
+        }
+        else if(SkyBoxChoice == SkyBoxes.Night)
+        {
+            RenderSettings.skybox = Night;
+        }
+	}
+	
+	
+}
